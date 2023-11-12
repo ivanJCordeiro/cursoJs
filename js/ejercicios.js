da929955395e6d51f48986b779ec48f502cbbab0
ivancordeiro <ivanjcordeiro94@gmail.com>
2023-11-12 16:24:20 -0300

    entregaFinalJs

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,47 +1,40 @@
 //FORMULARIO
 const formBusqueda = document.getElementById("busqueda");
 
-//INVOCACION DE BOTON SUBMIT Y FUNCION DE FILTRADO
-document.getElementById("busquedaBoton").addEventListener("click" , async (e) => {
+//INVOCACION DEL BOTON Y FUNCION DE FILTRADO
+document.getElementById("busquedaBoton").addEventListener("click", (e) => {
     e.preventDefault();
+    //TRAIGO EL INPUT, TOMO EL VALOR Y USO LOWERCASE PARA EVITAR ERRORES DE TIPEO
+    let musculosInput = document.getElementById("busquedaInput").value.toLowerCase();
+    //LLAMO EL ARRAY DE OBJETOS JSON
+    fetch(`../json/data.json`)
+        .then((response) => response.json())
+        .then((data) => {
+            let ejercicios = data.ejercicios;
 
-    //INPUT DONDE INGRESAMOS QUÉ BUSCAR
-    const musculosinput = document.getElementById("busquedaInput");
-    //LLAMO AL FETCH DEL ARCHIVO .JSON
-    let response = await fetch(`../json/data.json`);
-    //CONVIERTO LA RESPUESTA A .JSON()
-    let data = await response.json();
-    //FILTRADO FILTER IS NOT A FUNCTION ¿?
-    let filtroMusc = data.filter((el)=> el.musculo.includes(musculosinput.value));
-    let filtroDif = data.filter((el)=> el.dificultad.includes(musculosinput.value));
+            let filtroMusc = ejercicios.filter((el) => el.musculo && el.musculo.toLowerCase().includes(musculosInput));
+            let filtroDif = ejercicios.filter((el) => el.dificultad && el.dificultad.toLowerCase().includes(musculosInput));
 
-    //CONDICIONAL PARA CADA CASO DE RESPUESTA
-    if(
-        musculosinput.value === "espalda" ||
-        musculosinput.value === "pecho" || 
-        musculosinput.value === "cuadricep" ||
-        musculosinput.value === "femorales" ||
-        musculosinput.value === "gluteos" ||
-        musculosinput.value === "hombro" ||
-        musculosinput.value === "biceps" ||
-        musculosinput.value === "triceps"
-    ){
+            // CONCATENO RESULTADOS DE AMBOS FILTROS
+            let resultados = filtroMusc.concat(filtroDif);
 
-        let ejerciciosNombreMsg = "";
-        filtroMusc.forEach((el) => {
-            ejerciciosNombreMsg += `${ el.nombre }\n`
-        })
-        console.log(ejerciciosNombreMsg);
-    } else if (
-                musculosinput === "baja" ||
-                musculosinput === "media" ||
-                musculosinput === "alta"
-            ){
-            let ejerciciosDifMsg = "";
-            filtroDif.forEach ((el) => {
-                ejerciciosDifMsg += `${el.nombre}\n`
-            })
-            console.log(ejerciciosDifMsg);
-        }
+            // FILTRO DUPLICADOS POR SI HAY COINCIDENCIAS
+            let resultadosUnicos = Array.from(new Set(resultados.map((el) => el.nombre)));
+
+            // IMPRIMO RESULTADOS EN SWAL
+            if (resultadosUnicos.length > 0) {
+                let mensaje = resultadosUnicos.join("\n");
+                Swal.fire({
+                    title: "Ejercicios: ",
+                    text: mensaje,
+                    icon: "info"
+                  });
+            } else {
+                Swal.fire({
+                    icon: "error",
+                    text: "No se encontraron coincidencias",
+                  });
+            }
+        });
 });
 
